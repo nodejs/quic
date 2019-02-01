170c5d022ee1e53a847cfec22f315a6022c48a5f
James M Snell <jasnell@gmail.com>
2019-02-01 15:41:08 -0800

    deps: Add interface required to implement QUIC draft-17
    Ported from
    https://github.com/tatsuhiro-t/openssl/commit/920a331423f69f14b4871e35b476ea4fd573993a
    
    PR-URL: https://github.com/nodejs/quic/pull/6
    Reviewed-By: Daniel Bevenius <daniel.bevenius@gmail.com>

--- deps/openssl/openssl/ssl/ssl_lib.c
+++ deps/openssl/openssl/ssl/ssl_lib.c
@@ -4327,6 +4327,16 @@ void SSL_set_msg_callback(SSL *ssl,
     SSL_callback_ctrl(ssl, SSL_CTRL_SET_MSG_CALLBACK, (void (*)(void))cb);
 }
 
+void SSL_set_key_callback(SSL *ssl,
+                          int (*cb)(SSL *ssl, int name,
+                                    const unsigned char *secret,
+                                    size_t secretlen, void *arg),
+                          void *arg)
+{
+    ssl->key_callback = cb;
+    ssl->key_callback_arg = arg;
+}
+
 void SSL_CTX_set_not_resumable_session_callback(SSL_CTX *ctx,
                                                 int (*cb) (SSL *ssl,
                                                            int
