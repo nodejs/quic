4cbaa3ec86cd0b285ebd01cd1232647d337f9e10
James M Snell <jasnell@gmail.com>
2019-02-01 15:45:42 -0800

    deps: Remove EOED when SSL_MODE_QUIC_HACK is enabled
    Ported from
    https://github.com/tatsuhiro-t/openssl/commit/920a331423f69f14b4871e35b476ea4fd573993a

--- deps/openssl/openssl/ssl/statem/statem_clnt.c
+++ deps/openssl/openssl/ssl/statem/statem_clnt.c
@@ -450,7 +450,8 @@ static WRITE_TRAN ossl_statem_client13_write_transition(SSL *s)
         return WRITE_TRAN_CONTINUE;
 
     case TLS_ST_PENDING_EARLY_DATA_END:
-        if (s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
+        if (!(s->mode & SSL_MODE_QUIC_HACK)
+            && s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
             st->hand_state = TLS_ST_CW_END_OF_EARLY_DATA;
             return WRITE_TRAN_CONTINUE;
         }
--- deps/openssl/openssl/ssl/statem/statem_srvr.c
+++ deps/openssl/openssl/ssl/statem/statem_srvr.c
@@ -57,7 +57,8 @@ static int ossl_statem_server13_read_transition(SSL *s, int mt)
                 return 1;
             }
             break;
-        } else if (s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
+        } else if (!(s->mode & SSL_MODE_QUIC_HACK)
+                   && s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
             if (mt == SSL3_MT_END_OF_EARLY_DATA) {
                 st->hand_state = TLS_ST_SR_END_OF_EARLY_DATA;
                 return 1;
@@ -935,6 +936,15 @@ WORK_STATE ossl_statem_server_post_work(SSL *s, WORK_STATE wst)
                         SSL3_CC_APPLICATION | SSL3_CHANGE_CIPHER_SERVER_WRITE))
             /* SSLfatal() already called */
             return WORK_ERROR;
+
+            if ((s->mode & SSL_MODE_QUIC_HACK)
+                && s->ext.early_data == SSL_EARLY_DATA_ACCEPTED) {
+                s->early_data_state = SSL_EARLY_DATA_FINISHED_READING;
+                if (!s->method->ssl3_enc->change_cipher_state(
+                        s, SSL3_CC_HANDSHAKE | SSL3_CHANGE_CIPHER_SERVER_READ))
+                    /* SSLfatal() already called */
+                    return WORK_ERROR;
+            }
         }
         break;
 
