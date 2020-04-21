5f712f3383311eb8efe7e98715cdefa800d91e97
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-04-21 06:36:40 +0200

    quic: fix module_categories cannot_be_required set
    Currently, when configuring without quic support the code-cache test
    will fail. This is because the quic js files in lib/internal/quic are
    being added to the set of modules that cannot be required. When quic
    support is enabled NODE_EXPERIMENTAL_QUIC will be set in node.gyp.
    
    This commit updates the macro check to only check that
    NODE_EXPERIMENTAL_QUIC is defined.
    
    PR-URL: https://github.com/nodejs/quic/pull/375
    Reviewed-By: James M Snell <jasnell@gmail.com>

--- src/node_native_module.cc
+++ src/node_native_module.cc
@@ -104,7 +104,7 @@ void NativeModuleLoader::InitializeModuleCategories() {
       "internal/process/policy",
       "internal/streams/lazy_transform",
 #endif  // !HAVE_OPENSSL
-#if !defined(NODE_EXPERIMENTAL_QUIC) && NODE_EXPERIMENTAL_QUIC
+#if !NODE_EXPERIMENTAL_QUIC
       "internal/quic/core",
       "internal/quic/util",
 #endif
